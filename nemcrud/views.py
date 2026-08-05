7a63fcb234753fd8badfec31721f012d8c848b7b
juanzink7l <juanzink7l@gmail.com>
2026-08-05 17:30:57 -0300

    commitando

--- nemcrud/views.py
+++ nemcrud/views.py
@@ -6,16 +6,16 @@ def index(request):
     return render(request, "index.html")
 
 def cadastrar_evento(request):
-    # if request.method == "POST":
-    #     nome = request.POST.get('nome')
-    #     lotacao = request.POST.get('lotacao')
-    #     dia = request.POST.get('dia')
-    #     horario = request.POST.get('horario')
-    #     ingresso = request.POST.get('ingresso')
-    #     telefone = request.POST.get('telefone')
-    #     endereco = request.POST.get('endereco')
-    #     Evento.object.create(
-    #         nome=nome, lotacao=lotacao, dia=dia, horario=horario, ingresso=ingresso, telefone=telefone, endereco=endereco
-    #     )
-    #     return redirect('index')
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        lotacao = request.POST.get('lotacao')
+        dia = request.POST.get('dia')
+        horario = request.POST.get('horario')
+        ingresso = request.POST.get('ingresso')
+        telefone = request.POST.get('telefone')
+        endereco = request.POST.get('endereco')
+        Evento.objects.create(
+            nome=nome, lotacao=lotacao, dia=dia, horario=horario, ingresso=ingresso, telefone=telefone, endereco=endereco
+        )
+        return redirect('index')
     return render(request, 'cadastrar-evento.html')
